d7f8bf8f24bafb3fc643d5f968dccace1fccf1ab
Syahraz Fiqar Alamsyah <syahrazfa@gmail.com>
2026-01-06 19:10:32 +0700

    Initializing Data Pull

---
+++ extractors/tokopedia_html_sensor.py
@@ -0,0 +1,24 @@
+import requests
+import psycopg2
+
+url = "https://www.tokopedia.com/search?st=product&q=laptop"
+
+r = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+
+conn = psycopg2.connect(
+    dbname="tokopedia_dw",
+    user="postgres",
+    password="Jun218911",
+    host="localhost",
+    port=5432
+)
+
+cur = conn.cursor()
+
+cur.execute("""
+INSERT INTO bronze.tokopedia_raw (source, raw)
+VALUES (%s, %s)
+""", ("tokopedia_html_laptop", r.text))
+
+conn.commit()
+conn.close()
